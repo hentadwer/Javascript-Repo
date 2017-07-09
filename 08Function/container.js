63e1881ae64db1954f18fc3842b4d0263c12f54a
Jairo A Mejia R <sputnikweb@gmail.com>
2017-07-09 16:48:14 -0500

    Insert caracter after function calling

--- 08Function/container.js
+++ 08Function/container.js
@@ -1,21 +1,7 @@
-// the control structure switch help to compare many values in only one 
-// you are can add cases for comparation whit the structure switch
-// in the example i use fuel to compare priece and type 
-// remember define the var whit value do you need to compare 
+// this section is a making simple function for javascript 
 
-var fuel = "super100";
-
-switch (fuel) {
-  case "Diesel":
-    alert("1,25$");
-    break;
-  case "super98":
-    alert("1,40$");
-    break;
-  case "super99":
-    alert("1,50$");
-    break;
-    //you can add to the script default value, for inexisting keyword in the data base 
-  default:
-    alert("your keyword is not present in the catalog of fuel")
-}
+var a = 20;
+var b = 80;
+var c = 32;
+var d = 10;
+alert ("the value is ")
