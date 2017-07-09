5fd40f0cd41b4eb616360179255121f254d6bcdf
Jairo A Mejia R <sputnikweb@gmail.com>
2017-07-08 19:19:19 -0500

    Add New Folder for explaind switch.

---
+++ 07Switch/container.js
@@ -0,0 +1,14 @@
+// the section is for example use check balance credit bank founds whit complex conditions in Javascript
+/* The structure is only used per test and is not recommended for general use, 
+because there are better control structures in programming to differentiate values.
+*/
+var balance = 15000;
+
+if (balance >= 0) {
+  alert("the balance is positive");
+  if (balance > 1000 ) {
+    alert("the balance is very big")
+  }
+} else {
+  alert("the balance is negative");
+}
