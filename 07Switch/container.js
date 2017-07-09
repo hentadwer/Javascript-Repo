eb765fc96b88336811c4a1e762210e263ceaa532
Jairo A Mejia R <sputnikweb@gmail.com>
2017-07-08 19:40:04 -0500

    Add compare values whit break to stop and compare one by one.

--- 07Switch/container.js
+++ 07Switch/container.js
@@ -1,7 +1,16 @@
 // the control structure switch help to compare many values in only one 
 // you are can add cases for comparation whit the structure switch
 // in the example i use fuel to compare priece and type 
+// remember define the var whit value do you need to compare 
 
 switch (fuel) {
-
+  case "Diesel":
+    alert("1.25$");
+    break;
+  case "super98":
+    alert("1,40$");
+    break;
+  case "super99":
+    alert("1,50$");
+    break;
 }
