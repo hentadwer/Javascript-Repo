b0901f0dcc6e3a39e22ad7b62cab66b7b7c494b7
Jairo A Mejia R <sputnikweb@gmail.com>
2017-07-09 17:12:03 -0500

    Add Code And Comment the code

--- 08Function/container.js
+++ 08Function/container.js
@@ -1,7 +1,20 @@
 // this section is a making simple function for javascript 
 
-var a = 20;
-var b = 80;
-var c = 32;
-var d = 10;
-alert ("the value is ")
+function myScript () {
+  var a = 20;
+  var b = 80;
+  var c = 32;
+  var d = 10;
+  var e = a + b + c + d;
+  alert ("the value is equal to : " + e);
+}
+
+myScript();
+
+// this code function can call function any times Example:
+/*
+myScript();
+myScript();
+myScript();
+*/
+// the function execute three times the script menssage in the screen view.
