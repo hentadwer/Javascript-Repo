aa6d6f2263918606cda0273bacb0b3d301b064b7
Jairo A Mejia R <sputnikweb@gmail.com>
2017-07-08 19:49:44 -0500

    Update script add the default value in case of wrong keyword in data base

--- 07Switch/container.js
+++ 07Switch/container.js
@@ -3,9 +3,11 @@
 // in the example i use fuel to compare priece and type 
 // remember define the var whit value do you need to compare 
 
+var fuel = "super100";
+
 switch (fuel) {
   case "Diesel":
-    alert("1.25$");
+    alert("1,25$");
     break;
   case "super98":
     alert("1,40$");
@@ -13,4 +15,7 @@ switch (fuel) {
   case "super99":
     alert("1,50$");
     break;
+    //you can add to the script default value, for inexisting keyword in the data base 
+  default:
+    alert("your keyword is not present in the catalog of fuel")
 }
