fbe032a43d49f9d073e18ae2ed17c051c34a8b07
Jairo A Mejia R <sputnikweb@gmail.com>
2017-07-11 18:50:36 -0500

    Create folder explaind matrix interaction

---
+++ Example/Interaction/container.js
@@ -0,0 +1,11 @@
+// example for use condition IF in Java Script 
+var a = 5; 
+var b = 10;
+
+if (a < b) {
+  alert("They are not the same");
+}
+
+if (a == b) {
+  alert("If they are the same");
+}
