5a03706240f405e8261df7347c829e0fb8905c55
Jairo A Mejia R <sputnikweb@gmail.com>
2017-07-11 18:54:25 -0500

    add description

--- Example/Interaction/container.js
+++ Example/Interaction/container.js
@@ -1,11 +1,13 @@
-// example for use condition IF in Java Script 
-var a = 5; 
+/* this exercise is to explain the 
+interaction metod or search in to a matrix
+*/
+var a = 5;
 var b = 10;
 
 if (a < b) {
-  alert("They are not the same");
+    alert("They are not the same");
 }
 
 if (a == b) {
-  alert("If they are the same");
+    alert("If they are the same");
 }
