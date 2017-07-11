2090723ca8ae8ef4386251fe42b092ce1d7540e7
Jairo A Mejia R <sputnikweb@gmail.com>
2017-07-10 22:22:53 -0500

    Add while and conditions

--- Example/While/container.js
+++ Example/While/container.js
@@ -1,11 +1,12 @@
-// example for use condition IF in Java Script 
-var a = 5; 
-var b = 10;
 
-if (a < b) {
-  alert("They are not the same");
-}
+var quantity = 0;
 
-if (a == b) {
-  alert("If they are the same");
+// Create Index
+
+var i = 1;
+
+// Create condition
+
+while (i <= 10) {
+
 }
