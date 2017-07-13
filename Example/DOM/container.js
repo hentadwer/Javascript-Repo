fbb736607e56de21ffc24acfe6990ad62c767bbc
Jairo A Mejia R <sputnikweb@gmail.com>
2017-07-12 21:36:57 -0500

    Add Folder DOM to example

---
+++ Example/DOM/container.js
@@ -0,0 +1,20 @@
+// example for use operator equality in Java Script non strict == and strict ===
+
+var a = 123;
+var b = "123";
+
+// comprobation of equality non strict
+
+if (a == b) {
+    alert("if equality");
+} else {
+    alert("not equality");
+}
+
+// comprobation of equality very strict, evaluate value of the variable
+
+if (a === b) {
+    alert("if equality");
+} else {
+    alert("not equality");
+}
