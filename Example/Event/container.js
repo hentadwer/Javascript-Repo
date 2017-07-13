d3cfd9307c4b4f44abbbb41e8e93370aa46f6b08
Jairo A Mejia R <sputnikweb@gmail.com>
2017-07-12 22:26:53 -0500

    Add Programing Oriented for Events

---
+++ Example/Event/container.js
@@ -0,0 +1,6 @@
+// listen event is for Dom ejecute the event in document and launch in javaScript 
+// the event execute functions and messages and can modifi the element.
+var title = document.getElementById("myTitle");
+//alert(title);
+// this section add new content for the html document and print the new menssage. 
+title.innerHTML = "Is this a New Title";
