5f7652ce376c7fe2114db092be7a4a803011bb4b
Jairo A Mejia R <sputnikweb@gmail.com>
2017-07-12 22:35:25 -0500

    Add Event Listen and modify the h1 when mouse select H1

--- Example/Event/container.js
+++ Example/Event/container.js
@@ -1,6 +1,7 @@
 // listen event is for Dom ejecute the event in document and launch in javaScript 
 // the event execute functions and messages and can modifi the element.
+// this object name H1 it be weel modifed by the mouse cursor.
 var title = document.getElementById("myTitle");
-//alert(title);
-// this section add new content for the html document and print the new menssage. 
-title.innerHTML = "Is this a New Title";
+title.onclick = function() {
+    title.innerHTML = "Is this a New Title";
+}
