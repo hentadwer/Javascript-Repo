6b69e3df5b4cb2c774f4479ad81aae1fdc395f08
Jairo A Mejia R <sputnikweb@gmail.com>
2017-07-12 22:09:55 -0500

    Add Script in the Container.js and insert text H1 in html  a new title

--- Example/DOM/container.js
+++ Example/DOM/container.js
@@ -1,20 +1,4 @@
-// example for use operator equality in Java Script non strict == and strict ===
-
-var a = 123;
-var b = "123";
-
-// comprobation of equality non strict
-
-if (a == b) {
-    alert("if equality");
-} else {
-    alert("not equality");
-}
-
-// comprobation of equality very strict, evaluate value of the variable
-
-if (a === b) {
-    alert("if equality");
-} else {
-    alert("not equality");
-}
+var title = document.getElementById("myTitle");
+//alert(title);
+// this section add new content for the html document and print the new menssage. 
+title.innerHTML = "Is this a New Title";
